a3fdeabb76d3eab162b81c348c1249fa9e7f793a
PeteGoo <pete@petegoo.com>
2013-10-16 17:31:28 +1300

    initial commit

---
+++ ExtensionSDKConverter/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace MarkerMetro.ExtensionSDKConverter
+{
+    static class GuidList
+    {
+        public const string guidExtensionSDKConverterPkgString = "8557fba6-e4fa-42a2-9681-de6ed3fe735f";
+        public const string guidExtensionSDKConverterCmdSetString = "5ed3cedd-a531-4e2d-a519-37c13452b65b";
+
+        public static readonly Guid guidExtensionSDKConverterCmdSet = new Guid(guidExtensionSDKConverterCmdSetString);
+    };
+}
---
+++ ExtensionSDKConverter/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace MarkerMetro.ExtensionSDKConverter
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidConvertSdkToReference =        0x100;
+
+
+    };
+}
